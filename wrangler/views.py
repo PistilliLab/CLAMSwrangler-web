1c63b39012677b5ca0725212ca50a09e7b081ac2
sclayton33 <stu3cla@pm.me>
2023-12-01 13:12:55 -0500

    File upload to single unique directory working.

--- wrangler/views.py
+++ wrangler/views.py
@@ -35,14 +35,16 @@ def homepage_view(request):
 def upload_csv_files(request):
     if request.method == 'POST':
         # Generate unique ID for each upload session
-        upload_id = uuid.uuid4()
-        upload_dir = os.path.join(settings.MEDIA_ROOT, str(upload_id))
+        upload_id = str(uuid.uuid4())
+        upload_dir = os.path.join(settings.MEDIA_ROOT, upload_id)
 
         # Create directory for upload session
         os.makedirs(upload_dir, exist_ok=True)
 
-        # Upload all files
-        for file in request.FILES.getlist('file'):
+        # Iterate through the files received in the POST request
+        files = request.FILES.getlist('file')
+        for file in files:
+            # Save each file to the unique directory
             file_path = os.path.join(upload_dir, file.name)
             with open(file_path, 'wb+') as destination:
                 for chunk in file.chunks():
@@ -50,6 +52,8 @@ def upload_csv_files(request):
 
         # Save the file, process it, etc...
 
-        return JsonResponse({'message': 'File uploaded successfully'})
 
-    return JsonResponse({'error': 'Failed to upload file'}, status=400)
+        return JsonResponse({'message': 'File uploaded successfully', 'upload_id': upload_id})
+
+    else:
+        return JsonResponse({'error': 'Only POST method is allowed'}, status=405)
