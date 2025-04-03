d406220c6f2a9736287c46249e7fbbd6e124d4c1
sclayton33 <stu3cla@pm.me>
2025-04-03 19:33:40 -0400

    Actually fixed issue with static files paths for now.

--- CLAMS_web/settings/base.py
+++ CLAMS_web/settings/base.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+# STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 # Media files (user uploaded)
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
